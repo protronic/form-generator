f12969f1d257d9396407eeba81d5c211277bb829
Robert Seidler <robert.seidler1@googlemail.com>
2020-03-20 11:14:17 +0100

    Changed garantie to gewährleistung; Added a connection between choose-list model field and list filter;

--- bundle.js
+++ bundle.js
@@ -556,14 +556,20 @@ module.exports.InputFieldChooseList = class extends InputFieldText {
 
     genericLookUpQuery(this.options.queryUrl, '', this.options.listenQuery)
       .then(data => {
-        this.createChooseList(data);
-        this.querySelector(`input#${this.options.name}`).addEventListener('input', (event) => {
+        let resultInput = this.querySelector(`input#${this.options.name}`);
+        this.createChooseList(data, resultInput);
+        this.initialSelect(resultInput);
+        resultInput.addEventListener('input', (event) => {
           let formContainer = event.target.parentElement;
           if(event.target.value === ''){
-            formContainer.querySelector('.selected-item').classList.remove('selected-item');
-            formContainer.querySelector('.selected').classList.remove('selected');
-            formContainer.parentElement.createChooseList(data);
+            let item = formContainer.querySelector('.selected-item');
+            let selected = formContainer.querySelector('.selected');
+            if(item && selected){
+              item.classList.remove('selected-item');
+              selected.classList.remove('selected');  
+            }
           }
+          formContainer.parentElement.createChooseList(data, resultInput);
         });
       })
       .catch(err => {
@@ -575,24 +581,36 @@ module.exports.InputFieldChooseList = class extends InputFieldText {
     if(this.getModel()) this.formInputHandler({target: this});
   }
 
-  createChooseList(data){
+  initialSelect(resultInput){
+    let choose_list = this.querySelector('.choose-list');
+    choose_list.innerHTML = resultInput.value === '' ? this.orig_list_items.join('\n') : this.orig_list_items.filter(value => value.toLowerCase().includes(resultInput.value)).join('\n');
+    this.checkValidity()
+  }
+
+  createChooseList(data, resultInput){
     this.dbfailed = false;
     this.orig_list_items = data.map((entry, index) => `<li class="${index % 2 === 0 ? 'zebra1' : 'zebra2'}" onclick="((event) => { let compo = this.parentElement.parentElement.parentElement; console.log(compo); compo.querySelector('#${this.options.name}').value = event.target.value; compo.querySelector('ul').classList.add('selected-item'); compo.formInputHandler({target: compo}); compo.dispatchCustomEvent('form-input', event)})(event)" value="${entry[this.options.formWert]}">${Object.keys(entry).map(key => entry[key]).join(', ')}</li>`);
     let choose_list = this.querySelector('.choose-list');
-    choose_list.innerHTML = this.orig_list_items.join('\n');
-    this.querySelector('.filter-input').addEventListener('input', (event) => 
+    let filterInput = this.querySelector('.filter-input');
+    choose_list.innerHTML = resultInput.value === '' ? this.orig_list_items.join('\n') : this.orig_list_items.filter(value => value.toLowerCase().includes(resultInput.value)).join('\n');
+    filterInput.addEventListener('input', (event) => 
       (choose_list.innerHTML = this.orig_list_items.filter(value => value.toLowerCase().includes(event.target.value)).join('\n'))
     );
   }
 
   checkValidity(){
     let valid = super.checkValidity();
+    let model = this.getModel();
     [...this.querySelectorAll('li')].forEach(listItem => listItem.classList.remove('selected'));
-    let matchingListItem = this.querySelector(`li[value="${this.getModel()}"]`);
+    let matchingListItem = this.querySelector(`li[value="${model}"]`);
     if(matchingListItem) {
       let scrollTo = matchingListItem.offsetTop - matchingListItem.clientHeight -matchingListItem.offsetHeight;
       matchingListItem.parentElement.scrollTo({top: scrollTo})
       matchingListItem.classList.add('selected');
+      this.querySelector('ul').classList.add('selected-item')
+    } else {
+      console.log(model)
+      if(!this.dbfailed && (model !== undefined)) this.setValidityStatus(false, 'Kein entsprechender Wert in DB gefunden. Lösch die Eingabe und wähl ein Element aus der Liste aus')
     };
     if(this.dbfailed && valid) this.setValidityStatus(true, 'Datenbank nicht erreichbar.', true);
     return valid && ( this.dbfailed || matchingListItem );
