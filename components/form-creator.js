1705f4bb44ae4de352d405b07e799b00a888677e
Robert Seidler <robert.seidler1@googlemail.com>
2020-09-24 07:31:25 +0000

    added model['#modelID'] on loading the model

--- components/form-creator.js
+++ components/form-creator.js
@@ -172,6 +172,7 @@ function loadModelFromDB(modelId, formular){
         })
         .then(data => {
             console.log({model: data}); 
+            data['#modelID'] = modelId;
             return data
         })
 }
